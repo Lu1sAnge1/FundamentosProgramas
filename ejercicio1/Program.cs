d569df9419c500c2d4f9002b8a198bf0d322e415
Luis Angel <masteranggel@gmail.com>
2025-08-15 14:51:24 -0600

    ejercicio dia 1

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -1,3 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// TITULO
+// SEPARADOR
+// VIDA
+// MANA
 
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("PLAYER STATS");
+Console.WriteLine("------------");
+Console.WriteLine("HP       100");
+Console.WriteLine("MP       100");
+Console.WriteLine("GOLD   4,500");
+Console.WriteLine("EXP      300");
+Console.WriteLine("------------");
+
+Console.Write("PLAYER STATS \n------------\nHP      100 \nMP      100 \nGOLD   4,500 \nEXP      300 \n------------\n");
+Console.Write("PLAYER STATS \n------------\nHP\t100\nMP\t100\nGOLD\t4,500\nEXP\t300\n------------\n");
+
+Console.WriteLine();
+
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("MÉ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("XI");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("CO");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("MÉ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("XI");
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("CO");
+Console.ResetColor();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("RU");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("SS");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("IA");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("ALE");
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("MA");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("NIA");
+Console.ResetColor();
+
+

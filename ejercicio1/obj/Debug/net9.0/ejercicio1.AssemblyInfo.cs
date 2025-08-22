db4da18b10ec3921186324921f1ea5128e6859b8
Luis Angel <masteranggel@gmail.com>
2025-08-22 13:47:46 -0600

    configuracion de soluciones

--- ejercicio1/obj/Debug/net9.0/ejercicio1.AssemblyInfo.cs
+++ ejercicio1/obj/Debug/net9.0/ejercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9b17c104d45c406cb047a9defab0d59030a652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97138adf0f17e7da93ecc953da6fbce44f59cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

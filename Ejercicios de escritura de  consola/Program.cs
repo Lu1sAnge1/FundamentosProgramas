f89e224924e558ac49bef3a63e3736df65f84fb0
Luis Angel <masteranggel@gmail.com>
2025-08-22 14:15:52 -0600

    Ejercicio 2 imprimir datos

--- Ejercicios de escritura de  consola/Program.cs
+++ Ejercicios de escritura de  consola/Program.cs
@@ -1,25 +1,39 @@
 ﻿//ejercicios de escritura de consola
 #region Ejercicios
 
-//Ejericio #1
-ArbolDeNavidad();
+//Ejercicio #1
+//ArbolDeNavidad();
+//Ejercicio #2
+BiografiaDePersonaje();
 
 #endregion
 
 #region Soluciones
 
-//ejercicio #1
+//Ejercicio #1
 //generar un arbol de navidad en la consola
 void ArbolDeNavidad() {
-    Console.WriteLine("           *             ");
-    Console.WriteLine("          ( )            ");
-    Console.WriteLine("         (   )           ");
-    Console.WriteLine("        ( *   )           ");
-    Console.WriteLine("       (   * O )           ");
-    Console.WriteLine("      (    O *  )           ");
-    Console.WriteLine("     (___________)           ");
-    Console.WriteLine("         |  |                    ");
-    Console.WriteLine("         |  |                     ");
+    Console.WriteLine("           *           ");
+    Console.WriteLine("          ( )          ");
+    Console.WriteLine("         (   )         ");
+    Console.WriteLine("        ( *   )        ");
+    Console.WriteLine("       (   * O )       ");
+    Console.WriteLine("      (    O *  )      ");
+    Console.WriteLine("     (___________)     ");
+    Console.WriteLine("         |  |          ");
+    Console.WriteLine("         |  |          ");
 }
+//Ejercicio #2
+//imprimir datos de un personaje
+void BiografiaDePersonaje() {
+    string Nombre = "Shiki";
+    byte   Edad = 22;
+    string Ocupacion = "Hechicero";
+    string Ubicacion = "Torre del reloj";
 
+    Console.WriteLine("   Nombre :\t" + Nombre);
+    Console.WriteLine("   Edad :\t" + Edad);
+    Console.WriteLine("   Ocupacion :\t" + Ocupacion);
+    Console.WriteLine("   Ubicacion :\t" + Ubicacion);
+}
 #endregion

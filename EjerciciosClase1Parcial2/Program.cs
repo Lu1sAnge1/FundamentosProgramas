e6add1aa789cbb42ffb195bcba8e6bc68ff2b9ca
Luis Angel <masteranggel@gmail.com>
2025-09-26 14:28:49 -0600

    ejercicios clase 26/09/25

---
+++ EjerciciosClase1Parcial2/Program.cs
@@ -0,0 +1,38 @@
+﻿//ejercicios clase1 parcial 2
+
+#region Ejercicios
+
+PedirNombreEdad();
+
+
+#endregion
+
+
+
+
+
+
+#region Soluciones
+
+void PedirNombreEdad() {
+
+
+
+    Console.WriteLine("ingresa tu nombre");
+    var nombre = Console.ReadLine();
+
+    Console.WriteLine("ingresa tu edad");
+    var edad = Console.ReadLine();
+
+    var edadf = int.TryParse(edad, out var edadi);
+
+    var suma = edadi + 10;
+
+    Console.WriteLine($"Tu nombre es {nombre} y tu edad en 10 años sera {suma} años");
+
+
+
+}
+
+
+#endregion
